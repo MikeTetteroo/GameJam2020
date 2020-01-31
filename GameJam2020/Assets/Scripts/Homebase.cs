000f00d14b0229ed09db08ee118f373b9f9a6480
Marcus-Laptop <marcus.bicker@gmail.com>
2020-01-31 23:59:36 +0100

    Begin homebase

---
+++ GameJam2020/Assets/Scripts/Homebase.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Homebase : MonoBehaviour
+{
+    public Text UIPlaceText;
+    public GameObject[] collectibles;
+    public Collider deliverPoint;
+    // Start is called before the first frame update
+
+    public void PlaceCollectible(string collectibleName)
+    {
+        foreach (GameObject collectible in collectibles)
+        {
+            if(collectible.name == collectibleName)
+            {
+                collectible.SetActive(true);
+            }
+        }
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            StartCoroutine(TextFade(true));
+        }
+    }
+
+    public void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            StartCoroutine(TextFade(false));
+        }
+    }
+
+    public IEnumerator TextFade(bool fadeIn)
+    {
+        yield return new WaitForSeconds(0.2f);
+    }
+}
