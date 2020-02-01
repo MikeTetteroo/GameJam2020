71691bb0d22ccd454f264a24bce09f697a2ed87a
Daeneth <Daenethv@gmail.com>
2020-02-01 02:39:48 +0100

    collect particle effect

---
+++ GameJam2020/Assets/PickUpEffect.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpEffect : MonoBehaviour
+{
+    public ParticleSystem PickUpFirework;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Collectible")
+        {
+            Firework(other);
+        }
+    }
+
+    void Firework(Collider other)
+    {
+        ParticleSystem clone = Instantiate(PickUpFirework, other.transform.position, Quaternion.identity);
+    }
+}
