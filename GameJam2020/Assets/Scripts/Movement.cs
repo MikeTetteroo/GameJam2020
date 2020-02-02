53a8b62aa16e7baae5d39fb19793616314ce868a
Dennis Boon <dennis.boon96@gmail.com>
2020-02-02 11:32:21 +0100

    Fennek movement fixed with animations

--- GameJam2020/Assets/Scripts/Movement.cs
+++ GameJam2020/Assets/Scripts/Movement.cs
@@ -12,8 +12,9 @@ public class Movement : MonoBehaviour
     public float digSpeed = 4.0f;
     public float gravity = 20.0f;
     public string currentCollectible;
-    public GameObject Player;
+    public GameObject player;
     private Collider ground;
+    public Animator anim;
 
     private Vector3 moveDirection = Vector3.zero;
     private bool digging = false;
@@ -30,40 +31,55 @@ public class Movement : MonoBehaviour
     {
         if (characterController.isGrounded)
         {
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection = new Vector3(-Input.GetAxis("Vertical"), 0.0f, Input.GetAxis("Horizontal"));
             moveDirection = transform.TransformDirection(moveDirection);
+            //Debug.Log(transform.TransformDirection(moveDirection));
 
-            if (Input.GetButton("Running"))
+            if (Input.GetButton("Running") && characterController.velocity != Vector3.zero)
             {
+                StartCoroutine(PlayAnimation("Fennek_Run", 0.5f));
                 moveDirection *= runningSpeed;
-            } else
+            }
+            else if (characterController.velocity != Vector3.zero)
             {
+                StartCoroutine(PlayAnimation("Fennek_Walk", 0.667f));
                 moveDirection *= speed;
+            }
+            else
+            {
+                StartCoroutine(PlayAnimation("Fennek_Idle", 1.5f));
             }
 
             if (Input.GetButton("Jump"))
             {
+                StartCoroutine(PlayAnimation("Fennek_Jump", 0.817f));
                 moveDirection.y = jumpSpeed;
             }
 
             if (Input.GetButton("Dig") && digging == true)
             {
+                StartCoroutine(PlayAnimation("Fennek_Dig", 1.467f));
                 ground.gameObject.SetActive(false);
             }
             if(Input.GetButton("PlaceCollectible") && atDeposit == true)
             {
                 Homebase.Instance.PlaceCollectible(currentCollectible);
             }
+            //if (transform.TransformDirection(moveDirection) == new Vector3(0,0,0))
+            //{
+            //    StartCoroutine(PlayAnimation("Fennek_Idle", 1.5f));
+            //}
         }
             moveDirection.y -= gravity * Time.deltaTime;
 
         characterController.Move(moveDirection * Time.deltaTime);
+
+        player.transform.position = this.transform.position;
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "DiggableGround")
-        {
-            
+        {          
             ground = other;
             digging = true;
         }
@@ -76,5 +92,11 @@ public class Movement : MonoBehaviour
             ground = null;
             digging = false;
         }
+    }
+
+    IEnumerator PlayAnimation(string clip, float length)
+    {
+        anim.Play(clip);
+        yield return new WaitForSeconds(length);
     }
 }
